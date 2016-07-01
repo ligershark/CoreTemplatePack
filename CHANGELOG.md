20d3582678ddebd4ba2b63577c5353da2c5f8554
Mads Kristensen <post@madskristensen.net>
2016-07-01 14:27:56 -0700

    Add project files.

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Roadmap
+
+- [x] A feature that has been completed
+- [ ] A feature that has NOT yet been completed
+
+Features that have a checkmark are complete and available for
+download in the
+[CI build](http://vsixgallery.com/extension/ae9d6285-3f2a-4cbe-9021-82eb4d8b8c74/).
+
+# Changelog
+
+These are the changes to each version that has been released
+on the official Visual Studio extension gallery.
+
+## 1.1
+
+**2016-07-02**
+
+- [x] Feature 3
+- [x] Feature 4
+
+## 1.0
+
+**2016-07-01**
+
+- [x] Initial release
+- [x] Feature 1
+- [x] Feature 2
+  - [x] Sub feature
---
+++ Templates/BasicAspNetCore/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BasicAspNetCore
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
