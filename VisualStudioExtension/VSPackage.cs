5abbc87c64257abb55294d447c76d9fcbbfad47b
Mads Kristensen <post@madskristensen.net>
2016-07-05 11:32:46 -0700

    Added package to VSIX project

---
+++ VisualStudioExtension/VSPackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace CoreTemplatePack
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Name, IconResourceID = 400)]
+    [Guid("63b2059e-1711-455f-9d05-02ed4a439082")]
+    public sealed class VSPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
